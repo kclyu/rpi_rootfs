c3846bbcc9507235199b8b9c4a48ced439719899
Lyu,KeunChang <lyu.keun.chang@gmail.com>
2020-05-04 17:25:27 +0900

    Add libjpeg and libpulse dependency dev library

--- scripts/update_upgrade_install_package.sh
+++ scripts/update_upgrade_install_package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update
 apt full-upgrade -y
-apt install -y  gmodule-2.0 gtk+-3.0 libasound2-dev libavahi-client-dev
+apt install -y  gmodule-2.0 gtk+-3.0 libasound2-dev libavahi-client-dev libjpeg9-dev libpulse-dev
